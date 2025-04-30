963f15f65cdd74fb1f78846ed8fb5441a4cdc59e
Yorgo <yorgo.younan@gmail.com>
2025-04-30 15:20:28 +0300

    implemented round robin scheduling

--- src/scheduler/Main.java
+++ src/scheduler/Main.java
@@ -6,26 +6,41 @@ public class Main {
     	
     	Scheduler scheduler=new Scheduler();
     	
-        // Example of creating a process
+        //Create Processes
         Process p1 = new Process(1, 0, 5, 1);  // Arrival at time 0, burst time 5
-        Process p2 = new Process(2, 4, 3, 2);  // Arrival at time 1, burst time 3
-        Process p3= new Process(3, 2, 4, 1); 
-        Process p4 = new Process(4, 9, 2, 3);
-        Process p5 = new Process(5, 7, 6, 2);
+        Process p2 = new Process(2, 1, 3, 2);  // Arrival at time 1, burst time 3
+        Process p3= new Process(3, 2, 8, 1); 
+        Process p4 = new Process(4, 3, 6, 3);
+        //Process p5 = new Process(5, 10, 6, 2);
         
-      //Adding Processes to scheduler
-       scheduler.addProcess(p1);
-       scheduler.addProcess(p2);
-       scheduler.addProcess(p3);
-       scheduler.addProcess(p4);
-       scheduler.addProcess(p5);
+      // FCFS scheduling demo
+       Scheduler fcfs = new Scheduler();
+        
+       fcfs.addProcess(p1);
+       fcfs.addProcess(p2);
+       fcfs.addProcess(p3);
+       fcfs.addProcess(p4);
+       //fcfs.addProcess(p5);
 
        
        //running FCFS 
-       scheduler.runFCFS();
+       fcfs.runFCFS();
        
        //displaying results
-       scheduler.displayResults();
+       fcfs.displayResults("FCFS");
+       
+       
+       //RoundRobin Scheduling Demo
+       
+       Scheduler rr= new Scheduler();
+       
+       rr.addProcess(new Process(1, 0, 5, 1));
+       rr.addProcess(new Process(2, 1, 3, 2));
+       rr.addProcess(new Process(3, 2, 8, 1));
+       rr.addProcess(new Process(4, 3, 6, 3));
+       
+       rr.runRoundRobin(2); // time slice=2
+       rr.displayResults("Round-Robin q=2");
        
     }
 }
